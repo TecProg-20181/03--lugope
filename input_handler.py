10dc7d5941a2787442ccd2c032e819b7b04b7d3a
lugope <lucasgpereira.10@gmail.com>
2018-05-18 15:19:10 -0300

    Checking if the player's input is upper case.

--- input_handler.py
+++ input_handler.py
@@ -9,23 +9,31 @@ class InputHandler:
 			sys.exit()
 
 
+	# Get letter by input and verify if the letter belongs to the secret word
 	def getPlayerInput(self):
-		# Get letter by input and verify if the letter belongs to the secret word
 		letter = raw_input("Please guess a letter: ")
-		if letter in self.game.lettersGuessed:
-			print "Oops! You have already guessed that letter: ", self.game.getGessedWord()
 
-		elif letter in self.game.secretWord:
-			self.game.lettersGuessed.append(letter)
-			print "Good Guess: ", self.game.getGessedWord()
+		if self.isValidInput(letter):
+			if letter in self.game.lettersGuessed:
+				print "Oops! You have already guessed that letter: ", self.game.getGessedWord()
 
-		else:
-			self.game.loseOneGuess()
-			self.game.lettersGuessed.append(letter)
-			print "Oops! That letter is not in my word: ",  self.game.getGessedWord()
+			elif letter in self.game.secretWord:
+				self.game.lettersGuessed.append(letter)
+				print "Good Guess: ", self.game.getGessedWord()
+
+			else:
+				self.game.loseOneGuess()
+				self.game.lettersGuessed.append(letter)
+				print "Oops! That letter is not in my word: ",  self.game.getGessedWord()
 
 		print "------------"
 
-	
+	# Check if the input is a minuscle letter
+	def isValidInput(self, letter):
+		if letter.isupper():
+			print "Please, tipe a minuscle letter."
+			return False
 
-	
+		else:
+			return True
+
